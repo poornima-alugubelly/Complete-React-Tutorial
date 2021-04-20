9e7198af7ae9caedee0f0b1b4c34c08f0da97944
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-04-20 11:35:58 +0530

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
 const Home = () => {
+  // better to use blogs with hook beacause we might update the number of blogs and content and so on
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 }, //we defined id here beacuse we will use it later in map and this id shud be unique
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
   return (
     <div className="home">
+    // map goes thru each item in the object
       {blogs.map(blog => (
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
