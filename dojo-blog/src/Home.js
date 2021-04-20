862113eeaf93708fcf45e25de15851421cc67bda
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-04-20 15:54:36 +0530

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import BlogList from "./BlogList";
 
+// #1  we want to create a feature of delete blog button
+
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
@@ -8,6 +10,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  
+  // if id doesn't match then it is kept if it matches then removed, a new array based on this is created into newBlogs
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -15,6 +19,9 @@ const Home = () => {
 
   return (
     <div className="home">
+    
+    // here we pass handleDelete function as prop , we get doubt that y didn't we write this function in bloglist.js , but the data is here and we shud use that useState 
+    //for updation so we wrote it here.
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
