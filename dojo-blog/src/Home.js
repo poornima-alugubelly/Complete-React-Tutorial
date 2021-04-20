aeec81091a1b6dcf3aa4df5094c7264f2d2b545f
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-04-20 18:22:39 +0530

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -15,6 +15,11 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
+  // now we are going to fetch data for that we will use fake json server first create db.json file inside data folder in the json file the top most element is called the 
+  // resoource and using that we try to use create end point and use the data 
+  // The we should use json server packet to watch this data and unpack it we can do it either locally or by using npx
+  // second method is preferred --> open new terminal ---> npx json-server --watch data/db.json --port 8000 press enter now this will watch the db.json file 
+  //and wrap it in endpoints
   useEffect(() => {
     console.log('use effect ran');
     console.log(blogs);
