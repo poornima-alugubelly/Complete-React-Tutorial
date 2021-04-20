8cd2c3b3de14c2c1847f085e01350d17b5a57f1a
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-04-20 11:11:44 +0530

    Updated Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState } from "react"; // #3 have to import the hook
 
 const Home = () => {
-  // let name = 'mario';
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
+  // let name = 'mario'; #1 suppose we declared a variable  likes this
+  
+  const [name, setName] = useState('mario'); // first thing [name] is the value seconf thing is the fucntion to change it, parameter in useState is the intital value
+   const [age, setAge] = useState(25);
 
   const handleClick = () => {
-    // name = 'luigi';
+    // name = 'luigi';  #2 and we tried to change this like this if we see the home page now we will still see mario only beacuse the variable we created is not reactive 
+    // means react does'nt look at it for changes in state and hence the template is not updated and we have to make the value reactive so that react will see it and
+    // re render the template in the browser and for that we use a hook , hook is a special type of function that does a certain job it starts with use here we use useState hook 
     setName('luigi');
     setAge(30);
   }
