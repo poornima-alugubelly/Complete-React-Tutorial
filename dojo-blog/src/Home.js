0ca029a0cfdbd88e3825e9bb909403b4e9361882
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-04-20 13:21:17 +0530

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -8,9 +8,11 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  //we saw that in the previous code the blog part was here what if we want to reuse it? for that purpose we created the blogList file
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" /> // the blogs= {blogs} oart is called sending the prop because if we directly use the blog code in bloglist file we get error
+                                                   // as it doesn't know the blogs object
     </div>
   );
 }
