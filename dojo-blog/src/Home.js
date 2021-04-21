61bae9f47d2019406bd932876caa641c3061b1b0
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-04-21 12:21:15 +0530

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+// what if all the things we did in the previous lesson fetching, setting state and error handlers we want to do it in another part of our program too, then we will 
+// have to write it again so we externalize our code by writing it in a separate js file and importing it where we need this is called using Custom Hooks
 import useFetch from "./useFetch";
 
 const Home = () => {
