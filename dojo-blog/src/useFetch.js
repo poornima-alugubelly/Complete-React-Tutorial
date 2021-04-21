8742b7099913aac79e2e077b5fcaa046d31bfc06
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-04-21 12:33:20 +0530

    Update useFetch.js

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 
-const useFetch = (url) => {
-  const [data, setData] = useState(null);
+// #1 we should first create a function and then put all our code in it this fucntion acts as a hook (and it must always start with 'use'
+const useFetch = (url) => {  // #2 we put url here because in the previous lesson we hardcoded the url in the fetch() part but we might use this code for other urls as well
+  // so we put it here and also in fetch and also set it as the second parameter [url] below so that whenver url changes it is re-renders
+  // #3 we grabbed the states also because they are being updated int he code below
+  const [data, setData] = useState(null);   // #4 changed blogs to data because in future we might be using it for other things as well
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
@@ -14,7 +17,7 @@ const useFetch = (url) => {
         } 
         return res.json();
       })
-      .then(data => {
+      .then(data => {  // as this data variable is local it doesn't clash with the above ones
         setIsPending(false);
         setData(data);
         setError(null);
@@ -27,7 +30,7 @@ const useFetch = (url) => {
     }, 1000);
   }, [url])
 
-  return { data, isPending, error };
+  return { data, isPending, error };   // now this function should return something
 }
  
 export default useFetch;
